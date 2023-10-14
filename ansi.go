3d75cc9023be01dddc48da8a821e67fa3996a253
davidmdm <desmarais.michaud.david@gmail.com>
2023-10-13 23:49:52 -0400

    added basic spinner

--- ansi.go
+++ ansi.go
@@ -4,10 +4,24 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sync"
+	"time"
 )
 
 type Terminal struct {
 	io.Writer
+}
+
+func (term Terminal) Print(args ...any) {
+	_, _ = fmt.Fprint(term.Writer, args...)
+}
+
+func (term Terminal) Println(args ...any) {
+	_, _ = fmt.Fprintln(term.Writer, args...)
+}
+
+func (term Terminal) Printf(format string, args ...any) {
+	_, _ = fmt.Fprintf(term.Writer, format, args...)
 }
 
 func (term Terminal) SavePosition() {
@@ -40,6 +54,73 @@ func (term Terminal) ClearLineBeforeCursor() {
 
 func (term Terminal) ClearLine() {
 	_, _ = fmt.Fprint(term.Writer, "\u001B[2K")
+}
+
+type SpinnerOptions struct {
+	Chars           []string
+	InitialText     string
+	SpinnerInterval time.Duration
+	ClearAfterStop  bool
+}
+
+func (term Terminal) Spinner(opts SpinnerOptions) (chan<- string, func()) {
+	term.SavePosition()
+
+	if len(opts.Chars) == 0 {
+		opts.Chars = []string{"|", "/", "-", "\\"}
+	}
+
+	if opts.SpinnerInterval == 0 {
+		opts.SpinnerInterval = 650 * time.Millisecond
+	}
+
+	var (
+		i        int
+		wg       sync.WaitGroup
+		text     = opts.InitialText
+		charLen  = len(opts.Chars)
+		messages = make(chan string)
+		done     = make(chan struct{})
+	)
+
+	print := func() {
+		term.ResetCursor()
+		term.ClearAfterCursor()
+		term.Printf("%s %s", opts.Chars[i%charLen], text)
+	}
+
+	print()
+
+	wg.Add(1)
+	go func() {
+		ticker := time.NewTicker(opts.SpinnerInterval)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case text = <-messages:
+				print()
+			case <-ticker.C:
+				i++
+				ticker.Reset(opts.SpinnerInterval)
+				print()
+			case <-done:
+				wg.Done()
+				return
+			}
+		}
+	}()
+
+	stop := func() {
+		close(done)
+		wg.Wait()
+		if opts.ClearAfterStop {
+			term.ResetCursor()
+			term.ClearAfterCursor()
+		}
+	}
+
+	return messages, stop
 }
 
 var (
